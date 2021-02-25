baf43704d295d83834a85b4cbc650b12325e7f0d
Aryel <aryelmorinmercier@gmail.com>
2021-02-25 16:18:49 -0500

    On peut jouer
    yay

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -3,13 +3,21 @@
 public class PlayerMovement : MonoBehaviour
 {
     public float vitesseMouvement;
+    public float forceDeSaut;
+    public bool isJumping = false;
+    public bool auSol = true;
 
     public Rigidbody2D rb;
     private Vector3 velocite = Vector3.zero;
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         float mouvementHorizontal = Input.GetAxis("Horizontal") * vitesseMouvement * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
 
         DeplacerJoueur(mouvementHorizontal);
     }
@@ -18,6 +26,14 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 velociteCible = new Vector2(_mouvementHorizontal, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, velociteCible, ref velocite, .05f);
+
+        if (isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, forceDeSaut));
+            isJumping = false;
+        }
     }
 
 }
+
+
