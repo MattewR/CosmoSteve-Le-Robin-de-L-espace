3023108a20a9836d4a5292848ed930195b41db5d
Aryel <aryelmorinmercier@gmail.com>
2021-02-25 15:47:47 -0500

    MouvementCamera
    La caméra suit le personnage maintenant

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float vitesseMouvement;
+
+    public Rigidbody2D rb;
+    private Vector3 velocite = Vector3.zero;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float mouvementHorizontal = Input.GetAxis("Horizontal") * vitesseMouvement * Time.deltaTime;
+
+        DeplacerJoueur(mouvementHorizontal);
+    }
+
+    void DeplacerJoueur(float _mouvementHorizontal)
+    {
+        Vector3 velociteCible = new Vector2(_mouvementHorizontal, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, velociteCible, ref velocite, .05f);
+    }
+
+}
