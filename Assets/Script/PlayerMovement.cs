fcbd5d41b2280db2b8cd30a90153de6bbe4105cd
HonoriusGumuleux <bruny2001@hotmail.com>
2021-03-12 10:51:31 -0500

    Combiner Aryel au projet
    Ajouter le travail d'Aryel au projet

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float vitesseMouvement;
+    public float forceDeSaut;
+    public bool isJumping = false;
+    public bool auSol = true;
+
+    public Rigidbody2D rb;
+    private Vector3 velocite = Vector3.zero;
+    // Update is called once per frame
+    void Update()
+    {
+        float mouvementHorizontal = Input.GetAxis("Horizontal") * vitesseMouvement * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+
+        DeplacerJoueur(mouvementHorizontal);
+    }
+
+    void DeplacerJoueur(float _mouvementHorizontal)
+    {
+        Vector3 velociteCible = new Vector2(_mouvementHorizontal, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, velociteCible, ref velocite, .05f);
+
+        if (isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, forceDeSaut));
+            isJumping = false;
+        }
+    }
+
+}
+
+
