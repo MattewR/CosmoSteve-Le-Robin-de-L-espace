7f9adf870b4935152ac53abe43b3d7e7a6f69f06
HonoriusGumuleux <bruny2001@hotmail.com>
2021-03-16 10:07:13 -0400

    Début de partie
    Il est maintenant possible de commencer une partie depuis le menu principal

---
+++ Assets/Scripts/DemarrerJeu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DemarrerJeu : MonoBehaviour
+{
+    public void changer_niveau(string nom_scene)
+    {
+        SceneManager.LoadScene(nom_scene);
+    }
+}
