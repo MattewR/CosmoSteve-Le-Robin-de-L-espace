948e00c7059829ac5dc506c133c625699146c6bf
HonoriusGumuleux <bruny2001@hotmail.com>
2021-04-08 14:51:05 -0400

    Gravité niveau 2
    Début de la gravitée

---
+++ Assets/Scripts/InformationsNiveau2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InformationsNiveau2 : InformationsNiveau
+{
+    void Start()
+    {
+        setMassePlanete(7.342f * Mathf.Pow(10, 22));
+        setRayonPlanete(1737400);
+    }
+}
