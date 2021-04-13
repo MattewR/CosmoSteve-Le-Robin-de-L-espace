d55c21a5eb3df713e43c535d797af5b9a0efb3ef
Aryel <aryelmorinmercier@gmail.com>
2021-04-13 09:26:23 -0400

    Animations drapeau et checkpoint
    Voila

---
+++ Assets/Checkpoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public Animator animator;
+    private Transform playerSpawn;
+
+    private void Awake()
+    {
+        playerSpawn = GameObject.FindGameObjectWithTag("PlayerSpawn").transform;
+    }
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerSpawn.position = transform.position;
+            animator.SetBool("IsChecked", true);
+        }
+    }
+}
