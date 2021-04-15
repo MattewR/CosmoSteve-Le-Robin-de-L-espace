f3307ee4e9db0bc5a1446a267a3425d7e990296a
HonoriusGumuleux <bruny2001@hotmail.com>
2021-04-15 13:38:14 -0400

    Début du système de sauvegarde

---
+++ Assets/Scripts/Sauvegarde.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+
+public class Sauvegarde : MonoBehaviour
+{
+    //StreamWriter fichierDeSauvegarde = new("SauvegardPartie.txt", append: true);
+    
+    public void ecrire()
+    {
+        //fichierDeSauvegarde.Write("Allo");
+    }
+}
