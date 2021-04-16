02ee0b5655e95ba7b9eb26354975eb6bbd412d91
HonoriusGumuleux <bruny2001@hotmail.com>
2021-04-16 11:34:32 -0400

    L'astre se déplace
    Oo oO

---
+++ Assets/DeplacementAstre.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacementAstre : MonoBehaviour
+{
+    public bool test = false;
+    public Transform tranform;
+    public float vitesseDeplacement;
+    public Rigidbody2D rb;
+    private Vector3 velocite = Vector3.zero;
+    private Vector3 debutDeParcours = new Vector3(-40f, 20f, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(transform.position.x >= 178f)
+        {
+            transform.position = debutDeParcours;
+        }
+        Vector3 velociteCible = new Vector2(vitesseDeplacement * Time.deltaTime, 0);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, velociteCible, ref velocite, .05f);
+    }
+}
