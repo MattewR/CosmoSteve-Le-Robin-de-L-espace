6274be1cc393964e967750408fd7c36552bb0761
Aryel <aryelmorinmercier@gmail.com>
2021-04-29 16:28:07 -0400

    GameOverFonctionneYAY
    Knowledge of the day : nos intestins sont 6 mètres de long

--- Assets/GameOverManager.cs
+++ Assets/GameOverManager.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverManager : MonoBehaviour
 {
-/*
+
     public GameObject gameOverUI;
 
     public GameOverManager instance;
 
+    public string nom_scene;
 
     private void Awake()
     {
@@ -18,8 +20,26 @@ public class GameOverManager : MonoBehaviour
         instance = this;
     }
 
-    public void OnPlayerDeath()
+    public void RetryButton()
     {
-        gameOverUI.setActive(true);
-    }*/
+        //Recommencer le niveau 
+        SceneManager.LoadScene(nom_scene);
+        gameOverUI.SetActive(false);
+    }
+
+    public void MainMenuButton()
+    {
+        //Retour au menu principal
+
+        SceneManager.LoadScene("MenuPrincipal");
+
+        gameOverUI.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        //Fermer le jeu
+        Application.Quit();
+    }
+
 }
