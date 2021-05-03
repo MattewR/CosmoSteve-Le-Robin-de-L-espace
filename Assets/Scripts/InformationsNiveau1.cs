dc05e99f60c9b13d6477f665395aca5978787319
HonoriusGumuleux <bruny2001@hotmail.com>
2021-05-03 11:04:08 -0400

    Correction de petits bugs

--- Assets/Scripts/InformationsNiveau1.cs
+++ Assets/Scripts/InformationsNiveau1.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class InformationsNiveau1 : InformationsNiveau
 {
+    public GameObject[] cible;
+    public GameObject[] porte;
     override public void miseAJour()
     {
         setMassePlanete(5.972f * Mathf.Pow(10, 24));
         setRayonPlanete(6371000);
         setAstre(false);
+    }
+
+    public override void reinitialiser(Vector3 position)
+    {
+        for (int i = 0; i < cible.Length; i++)
+        {
+            cible[i].SetActive(true);
+        }
+
+        for (int i = 0; i < porte.Length; i++)
+        {
+            porte[i].SetActive(true);
+        }
     }
 }
