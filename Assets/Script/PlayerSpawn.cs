70645736ef24d75722c1b13a759a3a81b9c26ecc
HonoriusGumuleux <bruny2001@hotmail.com>
2021-05-06 10:39:25 -0400

    C'est presque correct
    Manque juste initialisation et remise à zéro

--- Assets/Script/PlayerSpawn.cs
+++ Assets/Script/PlayerSpawn.cs
@@ -2,8 +2,21 @@
 
 public class PlayerSpawn : MonoBehaviour
 {
+    public Sauvegarde sauvegarde;
+    private bool verifReprise;
+
     private void Awake()
     {
+        sauvegarde.Lire();
+        verifReprise = sauvegarde.GetVerificationReprise();
+        if (verifReprise == true)
+        {
+            transform.position =  new Vector3(sauvegarde.GetPositionX(), sauvegarde.GetPositionY(), 0);
+            Debug.Log(sauvegarde.GetPositionX());
+            Debug.Log(sauvegarde.GetPositionY());
+            sauvegarde.SetVerificationReprise(false);
+        }
+
         GameObject.FindGameObjectWithTag("Player").transform.position = transform.position;
     }
 }
--- Assets/Scripts/ReprendreUnePartie.cs
+++ Assets/Scripts/ReprendreUnePartie.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ReprendreUnePartie : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Sauvegarde sauvegarde;
 
-    // Update is called once per frame
-    void Update()
+    public void Changer_niveau()
     {
-        
+        sauvegarde.SetVerificationReprise(true);
+        SceneManager.LoadScene(sauvegarde.GetNiveau());
     }
 }
