0a07ba1691cf33f9b6f6c42c9051259d0fe0183a
HonoriusGumuleux <bruny2001@hotmail.com>
2021-05-18 09:34:56 -0400

    Ajustement de la gravité niveau 4

--- Assets/InformationsNiveau4.cs
+++ Assets/InformationsNiveau4.cs
@@ -6,8 +6,8 @@ public class InformationsNiveau4 : InformationsNiveau
 {
     override public void miseAJour()
     {
-        setMassePlanete(5f * 5.972f * Mathf.Pow(10, 24));
-        setRayonPlanete(1.5f * 6371000);
+        setMassePlanete(1.4f * 5.972f * Mathf.Pow(10, 24));
+        setRayonPlanete(1.17f * 6371000);
     }
 
 }
