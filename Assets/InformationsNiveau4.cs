a824c92c80986fc73d4fd272157614bf550fde45
HonoriusGumuleux <bruny2001@hotmail.com>
2021-05-18 09:05:20 -0400

    Correction bugs niveau 4

---
+++ Assets/InformationsNiveau4.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InformationsNiveau4 : InformationsNiveau
+{
+    override public void miseAJour()
+    {
+        setMassePlanete(5f * 5.972f * Mathf.Pow(10, 24));
+        setRayonPlanete(1.5f * 6371000);
+    }
+
+}
