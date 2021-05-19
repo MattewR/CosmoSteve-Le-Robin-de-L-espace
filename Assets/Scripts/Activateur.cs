eb1910eb7d746a4aaccbb65557e83179fbfa5eaf
HonoriusGumuleux <bruny2001@hotmail.com>
2021-05-19 16:32:26 -0400

    Ajustement de code
    Activateur et changement d'attraction

--- Assets/Scripts/Activateur.cs
+++ Assets/Scripts/Activateur.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//Cette classe permet à une cible de désactiver une porte lorsque la cible est touchée
 public class Activateur : MonoBehaviour
 {
+    //Variable
     public GameObject porte;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         this.gameObject.SetActive(false);
