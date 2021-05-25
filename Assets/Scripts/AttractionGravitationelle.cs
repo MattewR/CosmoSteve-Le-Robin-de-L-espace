ac0b54d4f3cdd0561ea01e5ad56efca9446a9e8a
HonoriusGumuleux <bruny2001@hotmail.com>
2021-05-25 11:32:16 -0400

    Ajustements du code
    Commentaires et méthodes ajoutés

--- Assets/Scripts/AttractionGravitationelle.cs
+++ Assets/Scripts/AttractionGravitationelle.cs
@@ -7,7 +7,7 @@ public class AttractionGravitationelle : MonoBehaviour
 {
     //Variables
     public Transform astre;
-    private float constanteGravitationnelle = 6.6742f * Mathf.Pow(10, -11);
+    private float constanteGravitationnelle = 6.6738f * Mathf.Pow(10, -11);
     public float distance2Rigidbody;
     public float distanceX;
     public float distanceY;
@@ -16,6 +16,7 @@ public class AttractionGravitationelle : MonoBehaviour
     public float mG;
     public float angle;
     public Vector2 forceAttraction;
+    private Vector2 vecteurUnitaire;
     private float masseAstre = 20000000000000;
 
     //Les valeurs constantes dans la formule gravitationnelle sont calculées au départ
@@ -28,20 +29,29 @@ public class AttractionGravitationelle : MonoBehaviour
     void FixedUpdate()
     {
         distance2Rigidbody = CalculDistance2();
-
         attraction = mG / distance2Rigidbody;
-        angle = Mathf.Atan2(distanceY, distanceX);
-        distanceX = Mathf.Cos(angle);
-        distanceY = Mathf.Sin(angle);
-        forceAttraction = new Vector2(distanceX, distanceY) * attraction;
+
+        vecteurUnitaire = CalculVecteurUnitaire();
+        forceAttraction = vecteurUnitaire * attraction;
         Steve.AddForce(forceAttraction);
     }
 
+    //Cette méthode calcule la distance au carré qui sépare le centre de l'astre et le centre de l'objet
     private float CalculDistance2()
     {
         distanceX = astre.position.x - this.transform.position.x;
         distanceY = astre.position.y - this.transform.position.y;
 
         return (Mathf.Pow(distanceX, 2) + Mathf.Pow(distanceY, 2));
+    }
+
+    //Cette méthode calcule le vecteur unitaire orienté vers le centre de l'astre 
+    private Vector2 CalculVecteurUnitaire()
+    {
+        angle = Mathf.Atan2(distanceY, distanceX);
+        distanceX = Mathf.Cos(angle);
+        distanceY = Mathf.Sin(angle);
+
+        return new Vector2(distanceX, distanceY);
     }
 }
--- Assets/Scripts/Gravite.cs
+++ Assets/Scripts/Gravite.cs
@@ -12,20 +12,16 @@ public class Gravite : MonoBehaviour
     public float massePlanete;
     private float constanteG = 6.6738f * Mathf.Pow(10, -11);
     private float rayonPlanete;
-    public float distanceJoueur;
-    public float distance;
 
-    // Start is called before the first frame update
+
+    // Start calcule la grandeur de la force ravitationnelle à appliquer à l'objet
     void Start()
     {
-        informationsNiveau.miseAJour();
-        massePlanete = informationsNiveau.getMassePlanete();
-        rayonPlanete = informationsNiveau.getRayonPlanete();
-        distanceJoueur = rb.position.y;
-        distance = distanceJoueur + rayonPlanete;
-        accelerationGravitationnelle = (constanteG * massePlanete) / Mathf.Pow(distance, 2);
+        InitialisationDesValeurs();
+        accelerationGravitationnelle = (constanteG * massePlanete) / Mathf.Pow(rayonPlanete, 2);
         forceGravitationnelle = new Vector2(0, accelerationGravitationnelle * -1)*rb.mass;
     }
+
 
     // Update is called once per frame
     void FixedUpdate()
@@ -33,13 +29,24 @@ public class Gravite : MonoBehaviour
         Attraction();
     }
 
+
+    //Cette méthode applique une force gravitationnelle vers la planète
     void Attraction()
     {
         rb.AddForce(forceGravitationnelle);
     }
 
+    //Cette méthode donne la valeur de l'accélération gravitationnelle
     public float getAccelerationGravitationnelle()
     {
         return accelerationGravitationnelle;
+    }
+
+    //Cette méthode initialise les informations propre à la planète
+    private void InitialisationDesValeurs()
+    {
+        informationsNiveau.miseAJour();
+        massePlanete = informationsNiveau.getMassePlanete();
+        rayonPlanete = informationsNiveau.getRayonPlanete();
     }
 }
